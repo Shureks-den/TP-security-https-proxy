414692b0b2b8adf493b20b351597feb104b04051
Александр Клонов <aleksandrklonov@MacBook-Air-Aleksandr.local>
2022-02-20 00:24:43 +0300

    Finished!!!!

--- README.md
+++
@@ -1,1 +0,0 @@
-# TP-security-https-proxy
--- task2/proxy.js
+++ task2/proxy.js
@@ -1,50 +1,114 @@
 import net from 'net'
+import tls from 'tls'
+import { spawn } from 'child_process';
+import fs from 'fs'
+import http from 'http'
+import url from 'url'
 
-const server = net.createServer();
+const certs = {};
+fs.readdirSync('certs/').forEach(file => {
+    certs[file.substring(0, file.length - 4)] = fs.readFileSync('certs/' + file)
+});
 
-server.on('connection', (clientProxySocket) => {
-    console.log('Client Connected To Proxy');
+const key = fs.readFileSync('selfsigned.key');
+const cert = fs.readFileSync('selfsigned.crt');
 
-    clientProxySocket.once('data', (data) => {
-        // ищем connect чтобы понять http или https
-        const isTLSConnection = data.toString().indexOf('CONNECT') !== -1;
-        let serverPort;
-        let serverHost;
-        if (isTLSConnection) {
-            serverPort = 443;
-            /*
-            Пришел запрос такого вида достаем из него адрес
-            CONNECT mail.ru:443 HTTP/1.1
-            Host: mail.ru:443
-            User-Agent: curl/7.64.1
-            Proxy-Connection: Keep-Alive
-            */
-            serverHost = data.toString().split('CONNECT ')[1].split(' ')[0].split(':')[0];
-        } else {
-            serverPort = 80;
-            serverHost = data.toString().split('Host: ')[1].split('\r\n')[0];
-        }
-        let proxyToServerSocket = net.createConnection({
-                host: serverHost,
-                port: serverPort
-            }, () => {
-            console.log('PROXY TO SERVER SET UP');
-            
-            if (isTLSConnection) {
-                clientProxySocket.write('HTTP/1.0 200 Connection established \r\n\n');
-            } else {
-                proxyToServerSocket.write(data);
+function createSecureContext(cert) {
+    return tls.createSecureContext({
+        key: key,
+        cert: cert
+    })
+}
+
+function generateCert(servername, cb) {
+    console.log(`gen cert ${servername}`);
+    let gen_cert = spawn('./gen_cert.sh', [servername, Math.floor(Math.random() * 1000000000000)]);
+
+    gen_cert.stdout.once('data', (data) => {
+        certs[servername] = data;
+        let ctx = createSecureContext(data);
+        cb(null, ctx);
+        fs.writeFile(`certs/${servername}.crt`, data, (err) => {
+            if (err) {
+                console.log(err.message)
             }
-
-            clientProxySocket.pipe(proxyToServerSocket);
-            proxyToServerSocket.pipe(clientProxySocket);
-            proxyToServerSocket.on('error', (err) => {
-                console.log(err);
-            });
-        });
-        clientProxySocket.on('error', (err) => {
-            console.log(err);
         })
+    });
+
+    gen_cert.stderr.on('data', (data) => {
+        console.log(`cert gen stderr: ${data}`)
+    })
+}
+
+function SNICallback(servername, cb) {
+    // console.log(`snicallback ${servername}`)
+    if (servername in certs) {
+        console.log(`using existing cert ${servername}`);
+        let ctx = createSecureContext(certs[servername]);
+        cb(null, ctx)
+    } else {
+        generateCert(servername, cb)
+    }
+}
+
+function httpConnection(req, res) {
+    if (req.url.startsWith('http')) {
+        try {
+            const parsedUrl = url.parse(req.url);
+            const options = {
+                host: parsedUrl.hostname,
+                port: 80
+            };
+            const proxyReq = net.connect(options, () => {
+
+                let h = '';
+                for (let i = 0; i < req.rawHeaders.length / 2; ++i) {
+                    if (req.rawHeaders[i * 2] === 'Proxy-Connection') {
+                        continue
+                    }
+                    h += `${req.rawHeaders[i * 2]}: ${req.rawHeaders[i * 2 + 1]}\r\n`;
+                }
+                let p = Buffer.from(`${req.method} ${parsedUrl.path} HTTP/1.1\r\n${h}\r\n`)
+                proxyReq.write(p);
+                req.socket.pipe(proxyReq).pipe(req.socket);        
+            });
+            proxyReq.on('error', (e) => {
+                console.log(`proxyReq error ${e}`)
+            })
+
+        } catch (e) {
+            console.log(`Unable to parse ${req.url}`)
+        }
+    }
+}
+
+const server = http.createServer(httpConnection);
+
+server.on('connect', (req, clientProxySocket, head) => {
+    console.log(`connect ${req.url}`);
+    let serverPort = req.url.split(':')[1];
+    let serverHost = req.url.split(':')[0];
+
+    let proxyToServerSocket = tls.connect({
+        host: serverHost,
+        port: serverPort
+    }, () => {
+    console.log('PROXY TO SERVER SET UP');
+    
+    clientProxySocket.write('HTTP/1.1 200 Connection established \r\n\n');
+
+    const tlsOptions = {
+        key: key,
+        cert: cert,
+        SNICallback: SNICallback,
+        isServer: true
+    };
+
+    const tlsSocket = new tls.TLSSocket(clientProxySocket, tlsOptions);
+    tlsSocket.pipe(proxyToServerSocket).pipe(tlsSocket);
+    });
+    clientProxySocket.on('error', (err) => {
+        console.log(err);
     })
 })
 
